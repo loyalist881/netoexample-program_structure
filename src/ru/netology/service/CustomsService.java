9a633ca69f2a047f8dd0db6da6360f10ab4a239f
loyalist881 <sokolovpavel31@gmail.com>
2025-09-22 13:28:39 +0700

    Update CustomsService.java

--- src/ru/netology/service/CustomsService.java
+++ src/ru/netology/service/CustomsService.java
@@ -1,14 +1,10 @@
 package ru.netology.service;
 
-public class CustomsService
-{
+public class CustomsService {
     static final int PRICE_PERCENTAGE = 100;
     static final int WEIGHT_RATE = 100;
 
-    public static int calculateCustoms(int productPrice, int productWeight)
-    {
-        int result = (productPrice / PRICE_PERCENTAGE) + (productWeight * WEIGHT_RATE);
-        System.out.println("Размер пошлины (в руб.) составит: " + result);
-        return result;
+    public static int calculateCustoms(int productPrice, int productWeight) {
+        return (productPrice / PRICE_PERCENTAGE) + (productWeight * WEIGHT_RATE);
     }
 }
